4ec3ae974857a735accf45dca5cc4722607f441f
Yash <anantyash.2710@gmail.com>
2024-12-16 17:24:38 +0530

    Install Material UI

--- AlumniDoor/src/components/SignUp.jsx
+++ AlumniDoor/src/components/SignUp.jsx
@@ -1,18 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
 
-const SignUp = () => {
-  return (
-    <View>
-      <Text >SignUp</Text>
-    </View>
-  )
+function SignUp() {
+  return <div>SignUp</div>;
 }
 
-export default SignUp
-
-const styles = StyleSheet.create({
-    divbg{
-        background-color: 
-    }
-})
+export default SignUp;
