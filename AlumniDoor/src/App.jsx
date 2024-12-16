49c89b13ce38e2074b2216549e7d41d920bbaf32
Yash <anantyash.2710@gmail.com>
2024-12-16 17:25:32 +0530

    create Navbar

--- AlumniDoor/src/App.jsx
+++ AlumniDoor/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Signin from './components/Signin'
+import SignUp from "./components/SignUp";
+import LandingPage from './Pages/LandingPage';
+
+import { StyledEngineProvider } from '@mui/material';
 
 function App() {
   return (
+    <StyledEngineProvider injectFirst>
+
     <div>
-      <Signin />
+      <LandingPage />
+      
     </div>
+    </StyledEngineProvider>
   )
 }
 
---
+++ AlumniDoor/src/components/NavBar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "../assets/logo2.png";
+import Button from "@mui/material/Button";
+
+function NavBar() {
+  return (
+    <div className="h-lvh w-lvw m-0 p-0">
+      <div className=" w-auto   flex items-center justify-between px-5 py-1"> {/*bg-green-300*/}
+        <div className="w-fit rounded-3xl flex-initial pt-2">
+          <img src={logo} alt="" srcset="" className="max-w-72" />
+        </div>
+        <nav className=" w-1/2">
+          <ul className="flex justify-evenly  font-bold list-none">
+            <li>
+              <Button variant="text" className="text-green-500 hover:bg-green-100 font-semibold">
+                Home
+              </Button>
+            </li>
+            <li>
+              <Button variant="text" className="text-green-500 hover:bg-green-100 font-semibold">
+                People
+              </Button>
+            </li>
+            <li>
+              <Button variant="text" className="text-green-500 hover:bg-green-100 font-semibold">
+                Door
+              </Button>
+            </li>
+            <li>
+              <Button variant="text" className="text-green-500 hover:bg-green-100 font-semibold">
+                College
+              </Button>
+            </li>
+            <li>
+              <Button variant="text" className="text-green-500 hover:bg-green-100 font-semibold">
+                Dashboard
+              </Button>
+            </li>
+          </ul>
+        </nav>
+        <div className="just">
+          <Button className="text-slate-50 bg-green-600 font-semibold hover:bg-green-400" variant="contained">
+            SignUp / Login
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
