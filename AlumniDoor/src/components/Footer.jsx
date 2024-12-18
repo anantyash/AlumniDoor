2f54c004eb8fe307986fa75b14d0ea868293f688
Yash <anantyash.2710@gmail.com>
2024-12-19 02:40:04 +0530

    Fixed width bug

--- AlumniDoor/src/components/Footer.jsx
+++ AlumniDoor/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="flex flex-col justify-center mt-10 h-fit w-lvw">
+    <div className="flex flex-col justify-center mt-10 h-fit w-full">
       <hr className="w-3/4 self-center mb-5 " />
 
       <p className=" self-center w-fit  text-zinc-400 pb-5">
