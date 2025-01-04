e992b8010f3e7123feb0096dac8c9b26922b7efa
Yash <anantyash.2710@gmail.com>
2025-01-05 03:19:44 +0530

    create indexs

---
+++ AlumniDoor/src/assets/iconIndex.js
@@ -0,0 +1,54 @@
+import CloseIcon from "@mui/icons-material/Close";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import MenuIcon from "@mui/icons-material/Menu";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import DoorBackIcon from "@mui/icons-material/DoorBack";
+import HomeIcon from "@mui/icons-material/Home";
+import SchoolIcon from "@mui/icons-material/School";
+import PeopleIcon from "@mui/icons-material/People";
+import Diversity1Icon from "@mui/icons-material/Diversity1";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
+import SendIcon from "@mui/icons-material/Send";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ChatIcon from "@mui/icons-material/Chat";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import LogoutIcon from "@mui/icons-material/Logout";
+import BadgeIcon from "@mui/icons-material/Badge";
+import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
+import Diversity2Icon from "@mui/icons-material/Diversity2";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+
+import { FaDonate } from "react-icons/fa";
+import { SiCodementor } from "react-icons/si";
+import { RiRobot2Fill } from "react-icons/ri";
+
+export {
+  CloseIcon,
+  NotificationsIcon,
+  LightModeIcon,
+  DarkModeIcon,
+  MenuIcon,
+  MeetingRoomIcon,
+  DoorBackIcon,
+  HomeIcon,
+  SchoolIcon,
+  PeopleIcon,
+  Diversity1Icon,
+  AddPhotoAlternateOutlinedIcon,
+  SendIcon,
+  DashboardIcon,
+  ChatIcon,
+  SearchIcon,
+  ArrowForwardIcon,
+  LogoutIcon,
+  BadgeIcon,
+  WorkspacePremiumIcon,
+  Diversity2Icon,
+  SupportAgentIcon,
+  FaDonate,
+  SiCodementor,
+  RiRobot2Fill,
+};
---
+++ AlumniDoor/src/assets/index.js
@@ -0,0 +1,127 @@
+import ALUMNIDOOR1 from "./AlumniPics/ALUMNIDOOR (1).png";
+import ALUMNIDOOR2 from "./AlumniPics/ALUMNIDOOR (2).png";
+import ALUMNIDOOR3 from "./AlumniPics/ALUMNIDOOR (3).png";
+import ALUMNIDOOR4 from "./AlumniPics/ALUMNIDOOR (4).png";
+import ALUMNIDOOR5 from "./AlumniPics/ALUMNIDOOR (5).png";
+import ALUMNIDOOR6 from "./AlumniPics/ALUMNIDOOR (6).png";
+import ALUMNIDOOR7 from "./AlumniPics/ALUMNIDOOR (7).png";
+import ALUMNIDOOR8 from "./AlumniPics/ALUMNIDOOR (8).png";
+import ALUMNIDOOR9 from "./AlumniPics/ALUMNIDOOR (9).png";
+import ALUMNIDOOR10 from "./AlumniPics/ALUMNIDOOR (10).png";
+import ALUMNIDOOR11 from "./AlumniPics/ALUMNIDOOR (11).png";
+import ALUMNIDOOR12 from "./AlumniPics/ALUMNIDOOR (12).png";
+import ALUMNIDOOR13 from "./AlumniPics/ALUMNIDOOR (13).png";
+import ALUMNIDOOR14 from "./AlumniPics/ALUMNIDOOR (14).png";
+import ALUMNIDOOR15 from "./AlumniPics/ALUMNIDOOR (15).png";
+import ALUMNIDOOR16 from "./AlumniPics/ALUMNIDOOR (16).png";
+import ALUMNIDOOR17 from "./AlumniPics/ALUMNIDOOR (17).png";
+import ALUMNIDOOR18 from "./AlumniPics/ALUMNIDOOR (18).png";
+import ALUMNIDOOR19 from "./AlumniPics/ALUMNIDOOR (19).png";
+import ALUMNIDOOR20 from "./AlumniPics/ALUMNIDOOR (20).png";
+import ALUMNIDOOR21 from "./AlumniPics/ALUMNIDOOR (21).png";
+import ALUMNIDOOR22 from "./AlumniPics/ALUMNIDOOR (22).png";
+import ALUMNIDOOR23 from "./AlumniPics/ALUMNIDOOR (23).png";
+import ALUMNIDOOR24 from "./AlumniPics/ALUMNIDOOR (24).png";
+import ALUMNIDOOR25 from "./AlumniPics/ALUMNIDOOR (25).png";
+import ALUMNIDOOR26 from "./AlumniPics/ALUMNIDOOR (26).png";
+import ALUMNIDOOR27 from "./AlumniPics/ALUMNIDOOR (27).png";
+import ALUMNIDOOR28 from "./AlumniPics/ALUMNIDOOR (28).png";
+import ALUMNIDOOR29 from "./AlumniPics/ALUMNIDOOR (29).png";
+import ALUMNIDOOR30 from "./AlumniPics/ALUMNIDOOR (30).png";
+import ALUMNIDOOR31 from "./AlumniPics/ALUMNIDOOR (31).png";
+import ALUMNIDOOR32 from "./AlumniPics/ALUMNIDOOR (32).png";
+import ALUMNIDOOR33 from "./AlumniPics/ALUMNIDOOR (33).png";
+import ALUMNIDOOR34 from "./AlumniPics/ALUMNIDOOR (34).png";
+import ALUMNIDOOR35 from "./AlumniPics/ALUMNIDOOR (35).png";
+import ALUMNIDOOR36 from "./AlumniPics/ALUMNIDOOR (36).png";
+import ALUMNIDOOR37 from "./AlumniPics/ALUMNIDOOR (37).png";
+import ALUMNIDOOR38 from "./AlumniPics/ALUMNIDOOR (38).png";
+import ALUMNIDOOR39 from "./AlumniPics/ALUMNIDOOR (39).png";
+import ALUMNIDOOR40 from "./AlumniPics/ALUMNIDOOR (40).png";
+import ALUMNIDOOR41 from "./AlumniPics/ALUMNIDOOR (41).png";
+import ALUMNIDOOR42 from "./AlumniPics/ALUMNIDOOR (42).png";
+import ALUMNIDOOR43 from "./AlumniPics/ALUMNIDOOR (43).png";
+import ALUMNIDOOR44 from "./AlumniPics/ALUMNIDOOR (44).png";
+import ALUMNIDOOR45 from "./AlumniPics/ALUMNIDOOR (45).png";
+import ALUMNIDOOR46 from "./AlumniPics/ALUMNIDOOR (46).png";
+import ALUMNIDOOR47 from "./AlumniPics/ALUMNIDOOR (47).png";
+import ALUMNIDOOR48 from "./AlumniPics/ALUMNIDOOR (48).png";
+import ALUMNIDOOR49 from "./AlumniPics/ALUMNIDOOR (49).png";
+import ALUMNIDOOR50 from "./AlumniPics/ALUMNIDOOR (50).png";
+import ALUMNIDOOR51 from "./AlumniPics/ALUMNIDOOR (51).png";
+import ALUMNIDOOR52 from "./AlumniPics/ALUMNIDOOR (52).png";
+import ALUMNIDOOR53 from "./AlumniPics/ALUMNIDOOR (53).png";
+import ALUMNIDOOR54 from "./AlumniPics/ALUMNIDOOR (54).png";
+import ALUMNIDOOR55 from "./AlumniPics/ALUMNIDOOR (55).png";
+import ALUMNIDOOR56 from "./AlumniPics/ALUMNIDOOR (56).png";
+import ALUMNIDOOR57 from "./AlumniPics/ALUMNIDOOR (57).png";
+import ALUMNIDOOR58 from "./AlumniPics/ALUMNIDOOR (58).png";
+import ALUMNIDOOR59 from "./AlumniPics/ALUMNIDOOR (59).png";
+import landingimg from "./landingimg.png";
+import logo from "../assets/logo1.png";
+import avatarimg from "../assets/avatarimg.png";
+
+export {
+  ALUMNIDOOR1,
+  ALUMNIDOOR2,
+  ALUMNIDOOR3,
+  ALUMNIDOOR4,
+  ALUMNIDOOR5,
+  ALUMNIDOOR6,
+  ALUMNIDOOR7,
+  ALUMNIDOOR8,
+  ALUMNIDOOR9,
+  ALUMNIDOOR10,
+  ALUMNIDOOR11,
+  ALUMNIDOOR12,
+  ALUMNIDOOR13,
+  ALUMNIDOOR14,
+  ALUMNIDOOR15,
+  ALUMNIDOOR16,
+  ALUMNIDOOR17,
+  ALUMNIDOOR18,
+  ALUMNIDOOR19,
+  ALUMNIDOOR20,
+  ALUMNIDOOR21,
+  ALUMNIDOOR22,
+  ALUMNIDOOR23,
+  ALUMNIDOOR24,
+  ALUMNIDOOR25,
+  ALUMNIDOOR26,
+  ALUMNIDOOR27,
+  ALUMNIDOOR28,
+  ALUMNIDOOR29,
+  ALUMNIDOOR30,
+  ALUMNIDOOR31,
+  ALUMNIDOOR32,
+  ALUMNIDOOR33,
+  ALUMNIDOOR34,
+  ALUMNIDOOR35,
+  ALUMNIDOOR36,
+  ALUMNIDOOR37,
+  ALUMNIDOOR38,
+  ALUMNIDOOR39,
+  ALUMNIDOOR40,
+  ALUMNIDOOR41,
+  ALUMNIDOOR42,
+  ALUMNIDOOR43,
+  ALUMNIDOOR44,
+  ALUMNIDOOR45,
+  ALUMNIDOOR46,
+  ALUMNIDOOR47,
+  ALUMNIDOOR48,
+  ALUMNIDOOR49,
+  ALUMNIDOOR50,
+  ALUMNIDOOR51,
+  ALUMNIDOOR52,
+  ALUMNIDOOR53,
+  ALUMNIDOOR54,
+  ALUMNIDOOR55,
+  ALUMNIDOOR56,
+  ALUMNIDOOR57,
+  ALUMNIDOOR58,
+  ALUMNIDOOR59,
+  landingimg,
+  logo,
+  avatarimg
+};
